ace6fdb4d9ef3936fc9a6daf8a98970be9b5f74c
Daniel Tigistu <166249731+codewithdann0@users.noreply.github.com>
2024-11-22 09:10:06 -0800

    day 2 of reactnative

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>This is the Home page!</Text>
     </View>
   );
 }
